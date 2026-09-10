4e100a56d14dbd81248e06e65cd2090726f72743
prablordeppey <prablordeppey@gmail.com>
2026-09-10 18:02:12 +0000

    fix assets dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   outDir: "./docs",
+  build: {
+    // 3. FIX THE 404: Rename the asset folder from '_astro' to 'assets'
+    // This stops GitHub Pages from blocking your files!
+    assets: "assets",
+  },
 });
